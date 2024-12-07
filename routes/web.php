0a1a507a567eefbe88fba90ae3dd8fe4808b809c
Soheil Saghian <soheilsaghian@gmail.com>
2024-12-08 01:27:47 +0330

    add sample page: account setting

--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,23 @@ Route::get('/', function () {
     // return view('dashboard.layouts.index');
 });
 
-Route::prefix('/dashboard')->group(function () {
+// Route::prefix('/dashboard')->group(function () {
+//     Route::get('/', function () {
+//         return view('dashboard.layouts.dashboard');
+//     })->middleware(['auth', 'verified'])->name('dashboard');
+// });
+
+Route::prefix('/console')->group(function () {
     Route::get('/', function () {
-        return view('dashboard.layouts.dashboard');
-    })->middleware(['auth', 'verified'])->name('dashboard');
+        return view('dashboard.dashboard');
+    })->name('dashboard');
 });
 
+Route::prefix('/console/sample')->group(function () {
+    Route::get('/acc-setting', function () {
+        return view('dashboard.sample.account-setting.pages-account-settings-account');
+    })->name('dashboard');
+});
 
 
 Route::middleware('auth')->group(function () {
